a138d0466a3b45947443a0f442c90188fa11308c
GouravJoshi19 <joshiguru09@gmail.com>
2024-01-31 17:22:03 +0530

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,14 @@ import pickle
 import streamlit as st
 import pandas as pd
 st.title("Diamond Price Predicter")
-model=pickle.load(open('E:\deployment\diamond.pkl','rb'))
+model=pickle.load(open("E:\deployment\diamond.pkl",'rb'))
 data = pd.read_csv("./diamonds.csv")
-carat=st.selectbox("Carat",data["carat"],index=None)
-cut=st.selectbox("cut",data["cut"],index=None)
-color=st.selectbox("color",data["color"],index=None)
-clarity=st.selectbox("clarity",data["clarity"],index=None)
-length=st.selectbox("length",data["x"],index=None)
-width=st.selectbox("width",data["y"],index=None)
-depth=st.selectbox("depth",data["z"],index=None)
+carat=st.text_input("Carat")
+cut=st.text_input("cut")
+length=st.text_input("length")
+width=st.text_input("width")
+depth=st.text_input("depth")
 result=''
 if st.button('Predict'):
-    result=model.predict([[carat,cut,color,clarity,length,width,depth]])[0]
+    result=model.predict([[carat,cut,length,width,depth]])[0]
 st.success(result)
