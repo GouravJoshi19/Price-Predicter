c0472c4283e8ad2bb7c7b3eda04c57751064aab9
GouravJoshi19 <joshiguru09@gmail.com>
2024-01-31 18:25:30 +0530

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ model=pickle.load(open("./diamond.pkl",'rb'))
 data = pd.read_csv("./diamonds.csv")
 carat = st.selectbox("Carat",data['carat'].unique(),index=None)
 cut= st.selectbox("cut",data['cut'].unique(),index=None)
-length = st.selectbox("length",data['x'].unique(),index=None)
-width = st.selectbox("width",data['y'].unique(),index=None)
-depth = st.selectbox("depth",data['z'].unique(),index=None)
+length = st.selectbox("length",data['x'].unique().astype(float),index=None)
+width = st.selectbox("width",data['y'].unique().astype(float),index=None)
+depth = st.selectbox("depth",data['z'].unique().astype(float),index=None)
 result=''
 if st.button('Predict'):
     result=model.predict([[carat,cut,length,width,depth]])
