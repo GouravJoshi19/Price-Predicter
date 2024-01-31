199552028cb4fda68d6e9311b144a139bb3b11cd
GouravJoshi19 <joshiguru09@gmail.com>
2024-01-31 18:17:42 +0530

    new commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ import numpy as np
 st.title("Diamond Price Predicter")
 model=pickle.load(open("./diamond.pkl",'rb'))
 data = pd.read_csv("./diamonds.csv")
-carat=st.text_input("Carat")
-cut=st.text_input("cut")
-length=st.text_input("length")
-width=st.text_input("width")
-depth=st.text_input("depth")
+carat = st.selectbox("Carat",data['carat'].unique(),index=None)
+cut= st.selectbox("cut",data['cut'].unique(),index=None)
+length = st.selectbox("length",data['x'].unique(),index=None)
+width = st.selectbox("width",data['y'].unique(),index=None)
+depth = st.selectbox("depth",data['z'].unique(),index=None)
 result=''
 if st.button('Predict'):
     result=model.predict([[carat,cut,length,width,depth]])
