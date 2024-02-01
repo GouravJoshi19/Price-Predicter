a7c38a24992e95e22fa8789dced488ea8e27682f
GouravJoshi19 <joshiguru09@gmail.com>
2024-02-01 12:52:46 +0530

    default

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,28 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
 st.title("Diamond Price Predicter")
 model=pickle.load(open("./diamond.pkl",'rb'))
 data = pd.read_csv("./diamonds.csv")
-carat = st.selectbox("Carat",data['carat'].unique(),index=None)
-length = st.selectbox("length",data['x'].unique().astype(float),index=None)
-width = st.selectbox("width",data['y'].unique().astype(float),index=None)
-depth = st.selectbox("depth",data['z'].unique().astype(float),index=None)
+carat = st.selectbox("Carat",options=data['carat'].sort_values().unique(),index=None)
+length = st.selectbox("length",data['x'].sort_values().unique().astype(float),index=None)
+width = st.selectbox("width",data['y'].sort_values().unique().astype(float),index=None)
+depth = st.selectbox("depth",data['z'].sort_values().unique().astype(float),index=None)
 result=''
+input_features=[carat,length,width,depth]
 if st.button('Predict'):
-    result=model.predict([[carat,length,width,depth]])
-st.success(result)
+           for i in input_features:
+             if(i==None):
+              st.write("please select all the features")
+              break
+           else:
+              result=model.predict([[carat,length,width,depth]])
+              st.write('price is:') 
+              st.success(result)
+              
+     
