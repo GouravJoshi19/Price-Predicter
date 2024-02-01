855cf843d60c5cfa56605f863ff5b54912cc6cfb
GouravJoshi19 <joshiguru09@gmail.com>
2024-02-01 13:54:28 +0530

    new layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,28 +2,32 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
+st.set_page_config(
+    page_title="Diamond Price Predictor",
+    page_icon="💎",
+    layout="centered",
+    initial_sidebar_state="collapsed"
+)
 st.title("Diamond Price Predicter")
+st.markdown("""This is a machine learning model which predicts the price of a diamond.
+            The features used here are **Carat** **Length** **Width** and **Depth** of the Diamond""")
 model=pickle.load(open("./diamond.pkl",'rb'))
 data = pd.read_csv("./diamonds.csv")
-carat = st.selectbox("Carat",options=data['carat'].sort_values().unique(),index=None)
-length = st.selectbox("length",data['x'].sort_values().unique().astype(float),index=None)
-width = st.selectbox("width",data['y'].sort_values().unique().astype(float),index=None)
-depth = st.selectbox("depth",data['z'].sort_values().unique().astype(float),index=None)
+st.sidebar.title("Input Features")
+carat = st.sidebar.selectbox("Carat",options=data['carat'].sort_values().unique(),index=None)
+length = st.sidebar.slider("Length(in mm)",float(data['x'].min()),float(data['x'].max()),float(data['x'].mean()))
+width = st.sidebar.slider("Width(in mm)",float(data['y'].min()),float(data['y'].max()),float(data['y'].mean()))
+depth = st.sidebar.slider("Depth(in mm)",float(data['z'].min()),float(data['z'].max()),float(data['z'].mean()))
 result=''
 input_features=[carat,length,width,depth]
+st.header("The features selected are:")
+st.write(pd.DataFrame(input_features))
 if st.button('Predict'):
-           for i in input_features:
-             if(i==None):
-              st.write("please select all the features")
-              break
-           else:
-              result=model.predict([[carat,length,width,depth]])
-              st.write('price is:') 
-              st.success(result)
-              
+       if any(Value is None for Value in input_features):
+           st.warning("Please Select All The Features")
+       else:
+            result=model.predict([[carat,length,width,depth]])
+            st.success(f'the price of the diamond is:${result[0]:.2f}')
+st.markdown("""----""")              
+st.markdown("""Made by :heart: Gourav Joshi""")
      
