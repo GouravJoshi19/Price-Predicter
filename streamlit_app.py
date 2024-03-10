baa48b2074342be0574d13c379d98a4ec37a0edb
GouravJoshi19 <gouravforcode@gmail.com>
2024-03-10 12:01:32 +0530

    new_updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,28 @@
 import pickle
-import streamlit as st
+import streamlit_app as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Loading the Data set and pickle model
+model=pickle.load(open("./diamond.pkl",'rb'))
+data = pd.read_csv("./diamonds.csv")
+
+
+
 st.set_page_config(
     page_title="Diamond Price Predictor",
     page_icon="💎",
     layout="centered",
     initial_sidebar_state="collapsed"
 )
+
+
 st.title(":orange[Diamond Price Predicter]")
 st.subheader("This is a machine learning model That predicts the price of a Diamond.")
-st.header(":orange[Description about the input features]")
+st.header(":orange[Description about the Dataset]")
+st.write("## Features ")
 st.markdown("""1.**Carat** -> Weight of the diamond, a significant factor in determining its value. \n\n
 2.**Cut(Quality)** -> Quality of cut(Fair, Good, Very Good, Premium, Ideal).\n\n
 3.**Color** -> Grading of the diamond's color, ranging from D (colorless) to Z (light yellow or brown).\n\n
@@ -22,8 +34,37 @@ st.markdown("""1.**Carat** -> Weight of the diamond, a significant factor in det
 7.**Width** -> Width of Diamond in mm. \n\n
 8.**Depth** -> Depth of Diamond in mm.\n\n
 """)
-model=pickle.load(open("./diamond.pkl",'rb'))
-data = pd.read_csv("./diamonds.csv")
+
+#Histogram
+st.write("## Carat distribution within the Data")
+sns.histplot(Data=data,x=data['carat'],bins=20,color='Green',kde=True)
+plt.title('Carat Distribution')
+st.pyplot()
+
+#Relation of Carat with the Target Feature
+st.write("## Relation between Carat and Price")
+sns.scatterplot(Data=data,x=data['carat'],y=data['price'],color='Yellow')
+plt.title('Carat-Price')
+st.pyplot()
+
+#Relation of various Parameters with Target Fetaure
+parameter=st.selectbox("Parameter",options=['Length','Width','Depth'])
+def name(string):
+     if(string=='Length'):
+          sns.scatterplot(Data=data,x=data['x'],y=data['price'],hue=data['y'])
+          plt.title('Length-Price')
+          st.pyplot()
+     elif(string=='width'):
+          sns.scatterplot(Data=data,x=data['y'],y=data['price'],hue=data['z'])
+          plt.title('Width-Price')
+          st.pyplot()
+     elif(string=='Depth'):
+          sns.scatterplot(Data=data,x=data['z'],y=data['price'],hue=data['x'])
+          plt.title('Depth-Price')
+          st.pyplot()
+
+name(parameter)
+
 st.sidebar.title("Input Features")
 cut_label={'Fair':0, 'Good':1, 'Very Good':2, 'Premium':3, 'Ideal':4}
 color_label={'J':0, 'I':1, 'H':2, 'G':3, 'F':4, 'E':5, 'D':6}
